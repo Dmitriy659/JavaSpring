0c02ce6455652c7ebcac6722660e3098e025f019
Dmitriy <dima.dima4554@gmail.com>
2024-04-09 09:36:21 +0300

    first

---
+++ src/main/java/org/dmitriy/dao/PersonDAO.java
@@ -0,0 +1,47 @@
+package org.dmitriy.dao;
+
+import org.dmitriy.models.Person;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Component
+public class PersonDAO {
+    private static int PEOPLE_COUNT;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+
+        people.add(new Person(++PEOPLE_COUNT, "Sasha", 20, "sashaDaun@mail.ru"));
+        people.add(new Person(++PEOPLE_COUNT, "Bogdan", 21, "BogdanBomj@gmail.com"));
+
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+
+    public void save(Person person) {
+        person.setId(++PEOPLE_COUNT);
+        people.add(person);
+    }
+
+    public void update(int id, Person updatedPerson) {
+        Person personToBeUpdated = show(id);
+
+        personToBeUpdated.setName(updatedPerson.getName());
+        personToBeUpdated.setAge(updatedPerson.getAge());
+        personToBeUpdated.setEmail(updatedPerson.getEmail());
+    }
+
+    public void delete(int id) {
+        people.removeIf(p -> p.getId() == id);
+    }
+}
